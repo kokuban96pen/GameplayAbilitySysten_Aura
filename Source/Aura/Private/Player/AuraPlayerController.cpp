35969e52431f8e2fdf05024cd98b0911deb38833
Kokuban819 <kokubanpaint@gmail.com>
2023-10-11 09:56:50 +0900

    Update EnhacedInput and PlayerControllerClass

--- Source/Aura/Private/Player/AuraPlayerController.cpp
+++ Source/Aura/Private/Player/AuraPlayerController.cpp
@@ -5,6 +5,7 @@
 
 //UEnhancedInputLocalPlayerSubsystem in EnhancedInputSubsystems.h
 #include "EnhancedInputSubsystems.h"
+#include "EnhancedInputComponent.h"
 
 AAuraPlayerController::AAuraPlayerController()
 {
@@ -34,4 +35,40 @@ void AAuraPlayerController::BeginPlay()
 
 	//Set InputModeData to InputMode of PlayerController
 	SetInputMode(InputModeData);
+}
+
+void AAuraPlayerController::SetupInputComponent()
+{
+	Super::SetupInputComponent();
+
+	UEnhancedInputComponent* EnhancedInputComponent = CastChecked<UEnhancedInputComponent>(InputComponent);
+
+	EnhancedInputComponent->BindAction(MoveAction, ETriggerEvent::Triggered, this, &AAuraPlayerController::Move);
+	
+}
+
+void AAuraPlayerController::Move(const FInputActionValue& InputActionValue)
+{
+	//Set Argument Value
+	const FVector2D InputAxisVector = InputActionValue.Get<FVector2D>();
+
+	//Get Current Rotation Value
+	const FRotator Rotator = GetControlRotation();
+
+	//Sey Current Yaw Rotation
+	const FRotator YawRotation(0.f,Rotator.Yaw,0.f);
+
+	//Create ForwardDirection From YawRotation
+	//GetUnitAxis return normalized vector
+	const FVector ForwardDirection = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::X);
+	const FVector RightDirection = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::Y);
+
+	//Not Using CheckFunction because This Function call Tick  
+	if (APawn* ControlledPawn = GetPawn<APawn>())
+	{
+		//Input Value Setting to  Movement Input of Character
+		ControlledPawn->AddMovementInput(ForwardDirection, InputAxisVector.Y);
+		ControlledPawn->AddMovementInput(RightDirection, InputAxisVector.X);
+	}
+	
 }
